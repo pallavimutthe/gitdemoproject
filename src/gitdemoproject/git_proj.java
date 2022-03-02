2e6efb2005e2f57166ad72f644a7f18c1935a64f
ADMIN <ADMIN@DESKTOP-74OCNT1>
2022-03-02 12:56:27 +0530

    message from git S

---
+++ src/gitdemoproject/git_proj.java
@@ -0,0 +1,10 @@
+package gitdemoproject;
+
+public class git_proj {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello from git");
+	}
+
+}
